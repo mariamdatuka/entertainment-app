9dd0a2bd1042cbd9c959ba6655e8a117bfd69384
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-07-26 12:29:23 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter,RouterProvider } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+
+
+ 
+const router=createBrowserRouter([
+    {
+      path:'/',
+      element:<Homepage/>
+    },
+])
+
+
+
+function App() {
+
+
+  return (
+    <>
+       <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
