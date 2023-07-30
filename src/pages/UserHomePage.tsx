bf21bf775363fe373d5b76eec858f78e3d66ad41
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-07-31 01:02:32 +0400

    create homepage and fetch the data

--- src/pages/UserHomePage.tsx
+++ src/pages/UserHomePage.tsx
@@ -1,7 +1,24 @@
+import {useState,useEffect} from 'react'
 import Layout from "../components/Layout";
 import Card from "../components/Card";
+import axios from 'axios'
 
 const UserHomePage = () => {
+  const [items,setItems]=useState<any>([])
+  const url= 'https://entertainment-web.onrender.com/api/film'
+   
+  const fetchAllItems=async()=>{
+    try {
+      const response=await axios.get(url);
+      setItems(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+     fetchAllItems();
+  },[])
   return (
    
    <>
@@ -11,13 +28,22 @@ const UserHomePage = () => {
             type='text' placeholder='Search for movies or TV series'/>
         </form>
         <h3 className='text-xl py-2'>Trending</h3>
-        <section className='overflow-x-auto flex space-x-4'>
+        <section className='overflow-x-auto flex gap-4 mb-4'>
              <Card/>
              <Card/>
              <Card/>
              <Card/>
         </section>
-      
+       <h3 className='text-xl py-2'>Recommended for you</h3>
+       <section className='grid grid-cols-2'>
+            {
+              items.map((itm:any)=>(
+                 <div key={itm.id}>
+                     {itm.title}
+                 </div>
+              ))
+            }
+       </section>
      </Layout>
    </>
    
